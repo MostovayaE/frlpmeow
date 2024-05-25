ed8dc20212363c3498f169d8649b3acff03fd13f
BaranovaAnastasia17 <112949430+BaranovaAnastasia17@users.noreply.github.com>
2024-05-25 04:54:32 +0300

    Add files via upload

--- src/mire/commands.clj
+++ src/mire/commands.clj
@@ -1,117 +1,50 @@
 (ns mire.commands
   (:require [clojure.string :as str]
             [mire.rooms :as rooms]
-            [mire.player :as player]))
+            [mire.player :as player]
+            [mire.classes :as classes]
+            [mire.shared :as shared]))
 
-(defn- move-between-refs
-  "Move one instance of obj between from and to. Must call in a transaction."
-  [obj from to]
-  (alter from disj obj)
-  (alter to conj obj))
+;; Динамическая переменная для хранения роли игрока
+(def ^:dynamic *role* nil)
 
-;; Command functions
+;; Функция для получения строки с ролью пользователя
+(defn get-player-role-string [player-name]
+  (let [role (shared/get-player-role player-name)]
+    (if role
+      (str "Your current role is: " (name role))
+      "Role is not set yet.")))
 
-(defn look
-  "Get a description of the surrounding environs and its contents."
-  []
-  (str (:desc @player/*current-room*)
-       "\nExits: " (keys @(:exits @player/*current-room*)) "\n"
-       (str/join "\n" (map #(str "There is " % " here.\n")
-                           @(:items @player/*current-room*)))))
+(defn get-player-class-string [player-name]
+  (let [class (shared/get-player-class player-name)]
+    (if class
+      (str "Your current class is: " class)
+      "Class is not set yet.")))
 
-(defn move
-  "\"♬ We gotta get out of this place... ♪\" Give a direction."
-  [direction]
-  (dosync
-   (let [target-name ((:exits @player/*current-room*) (keyword direction))
-         target (@rooms/rooms target-name)]
-     (if target
-       (do
-         (move-between-refs player/*name*
-                            (:inhabitants @player/*current-room*)
-                            (:inhabitants target))
-         (ref-set player/*current-room* target)
-         (look))
-       "You can't go that way."))))
+;; Функция для выполнения команд
+(defn execute-command [input player-name]
+  (let [player-role (shared/get-player-role player-name)
+        command (str/split input #" ")]
+    (if (= player-role :player) ; Проверяем, что роль - player
+      (case (first command)
+        "role" (get-player-role-string player-name)
+        "class" (get-player-class-string player-name)
+        "start" (do
+                  (shared/add-ready-player player-name)
+                  (let [ready-count (shared/ready-player-count)]
+                    (if (= ready-count shared/max-connections)
+                      (shared/broadcast-message "All players are ready. The game is starting!")
+                      (str ready-count " players are ready."))))
+        "Unknown command")
+      "Access denied. You must be a player to execute commands.")))
 
-(defn grab
-  "Pick something up."
-  [thing]
-  (dosync
-   (if (rooms/room-contains? @player/*current-room* thing)
-     (do (move-between-refs (keyword thing)
-                            (:items @player/*current-room*)
-                            player/*inventory*)
-         (str "You picked up the " thing "."))
-     (str "There isn't any " thing " here."))))
-
-(defn discard
-  "Put something down that you're carrying."
-  [thing]
-  (dosync
-   (if (player/carrying? thing)
-     (do (move-between-refs (keyword thing)
-                            player/*inventory*
-                            (:items @player/*current-room*))
-         (str "You dropped the " thing "."))
-     (str "You're not carrying a " thing "."))))
-
-(defn inventory
-  "See what you've got."
-  []
-  (str "You are carrying:\n"
-       (str/join "\n" (seq @player/*inventory*))))
-
-(defn detect
-  "If you have the detector, you can see which room an item is in."
-  [item]
-  (if (@player/*inventory* :detector)
-    (if-let [room (first (filter #((:items %) (keyword item))
-                                 (vals @rooms/rooms)))]
-      (str item " is in " (:name room))
-      (str item " is not in any room."))
-    "You need to be carrying the detector for that."))
-
-(defn say
-  "Say something out loud so everyone in the room can hear."
-  [& words]
-  (let [message (str/join " " words)]
-    (doseq [inhabitant (disj @(:inhabitants @player/*current-room*)
-                             player/*name*)]
-      (binding [*out* (player/streams inhabitant)]
-        (println message)
-        (println player/prompt)))
-    (str "You said " message)))
-
-(defn help
-  "Show available commands and what they do."
-  []
-  (str/join "\n" (map #(str (key %) ": " (:doc (meta (val %))))
-                      (dissoc (ns-publics 'mire.commands)
-                              'execute 'commands))))
-
-;; Command data
-
-(def commands {"move" move,
-               "north" (fn [] (move :north)),
-               "south" (fn [] (move :south)),
-               "east" (fn [] (move :east)),
-               "west" (fn [] (move :west)),
-               "grab" grab
-               "discard" discard
-               "inventory" inventory
-               "detect" detect
-               "look" look
-               "say" say
-               "help" help})
-
-;; Command handling
-
+;; Основная функция для обработки команд
 (defn execute
   "Execute a command that is passed to us."
-  [input]
-  (try (let [[command & args] (.split input " +")]
-         (apply (commands command) args))
-       (catch Exception e
-         (.printStackTrace e (new java.io.PrintWriter *err*))
-         "You can't do that!")))
+  [input player-name]
+  (try
+    (let [[command & args] (str/split input #" +")]
+      (execute-command command player-name))
+    (catch Exception e
+      (.printStackTrace e (new java.io.PrintWriter *err*))
+      "You can't do that!")))
--- src/mire/server.clj
+++ src/mire/server.clj
@@ -1,18 +1,68 @@
 (ns mire.server
   (:require [clojure.java.io :as io]
+            [clojure.string :as str]
             [server.socket :as socket]
             [mire.player :as player]
             [mire.commands :as commands]
-            [mire.rooms :as rooms]))
+            [mire.rooms :as rooms]
+            [mire.classes :as classes :refer [get-current-class]]
+            [mire.shared :as shared]))
 
-(defn- cleanup []
-  "Drop all inventory and remove player from room and player list."
+; defs
+(def current-connections (atom 0))
+(def max-connections 2)
+(def player-roles (atom {})) ; Сохранение информации о роли каждого игрока
+(def player-classes (atom {}))
+(def ready-players (atom #{}))  ; Множество готовых игроков
+
+(defn broadcast-message [message]
+  (doseq [out (vals @player/streams)]
+    (binding [*out* out]
+      (println message)
+      (flush))))
+
+(defn broadcast-message-to-players [message]
+  (doseq [[name out] @player/streams]
+    (when (= (:role (@player-roles name)) :player)
+      (binding [*out* out]
+        (println message)
+        (flush)))))
+
+(defn get-player-class []
+  (print "\nChoose your class (Vagabound, Ranger, Vagrant, Warrior, Scounderl): ") (flush)
+  (let [class (read-line)]
+    (if (some #{"Vagabound" "Ranger" "Vagrant" "Warrior" "Scounderl"} (str/split class #" "))
+      class
+      (do
+        (println "Invalid class. Please choose again.")
+        (recur)))))
+
+(defn- get-player-role [is-spectator]
+  (if is-spectator
+    :spectator
+    (do
+      (print "Do you want to play? (Yes/No): ") (flush)
+      (let [response (clojure.string/lower-case (read-line))]
+        (cond
+          (= response "yes") :player
+          (= response "no") :spectator
+          :else (do
+                  (println "Invalid response. Please answer Yes or No.")
+                  (recur false)))))))
+                  
+; END NEW
+
+(def ^:dynamic *role*)
+
+(defn- cleanup [player-name]
   (dosync
-   (doseq [item @player/*inventory*]
-     (commands/discard item))
-   (commute player/streams dissoc player/*name*)
-   (commute (:inhabitants @player/*current-room*)
-            disj player/*name*)))
+   (commute player/streams dissoc player-name))
+  (swap! player-roles dissoc player-name)
+  (when (= *role* :player)
+    (swap! current-connections dec)
+    (swap! ready-players disj player-name))
+  (broadcast-message (str player-name " has disconnected. Current players: " @current-connections))
+  (println "Player disconnected. Current players:" @current-connections))
 
 (defn- get-unique-player-name [name]
   (if (@player/streams name)
@@ -21,35 +71,73 @@
         (recur (read-line)))
     name))
 
+(defn start-game []
+  (when (= (count @ready-players) max-connections)
+    (broadcast-message "All players are ready. The game has started!")
+    ))
+
+(defn execute-command [input]
+  (if (= input "start")
+    (do
+      (swap! ready-players conj player/*name*)
+      (broadcast-message-to-players (str player/*name* " is ready."))
+      (let [chosen-class (get-player-class)]
+        (start-game)))
+    (commands/execute input)))
+
+
+(defn- get-current-class-command []
+  (get-current-class @player-roles player/*name*))
+
+
 (defn- mire-handle-client [in out]
-  (binding [*in* (io/reader in)
-            *out* (io/writer out)
-            *err* (io/writer System/err)]
+  (let [is-spectator (>= @current-connections shared/max-connections)]
+    (binding [*in* (io/reader in)
+              *out* (io/writer out)
+              *err* (io/writer System/err)]
+      (print "\nWhat is your name? ") (flush)
+      (let [player-name (get-unique-player-name (read-line))
+            player-role (get-player-role is-spectator)
+            player-class (if (= player-role :player) (get-player-class) nil)]
+        (dosync
+          (commute player/streams assoc player-name *out*)
+          (shared/set-player-role player-name player-role)
+          (when player-class
+            (shared/set-player-class player-name player-class))) ; Сохранение класса игрока, если применимо
+        (when (= player-role :player)
+          (swap! current-connections inc)
+          (let [role-msg (str player-name " has connected as a player"
+                              (if player-class (str " with class " player-class ".") "."))]
+            (shared/broadcast-message (str role-msg " Current players: " @current-connections)))
 
-    ;; We have to nest this in another binding call instead of using
-    ;; the one above so *in* and *out* will be bound to the socket
-    (print "\nWhat is your name? ") (flush)
-    (binding [player/*name* (get-unique-player-name (read-line))
-              player/*current-room* (ref (@rooms/rooms :start))
-              player/*inventory* (ref #{})]
-      (dosync
-       (commute (:inhabitants @player/*current-room*) conj player/*name*)
-       (commute player/streams assoc player/*name* *out*))
+          ;; Проверка, достигло ли количество подключений max-connections
+          (when (= @current-connections shared/max-connections)
+            (shared/broadcast-message "All players have connected. Type 'start' to begin the game!")))
 
-      (println (commands/look)) (print player/prompt) (flush)
+        (print player/prompt) 
+        (flush)
 
-      (try (loop [input (read-line)]
-             (when input
-               (println (commands/execute input))
-               (.flush *err*)
-               (print player/prompt) (flush)
-               (recur (read-line))))
-           (finally (cleanup))))))
+        (try
+          (loop [input (read-line)]
+            (when input
+              (let [response (commands/execute input player-name)]
+                (when (not= response "Unknown command")
+                  (shared/broadcast-message response))  ;; Передаем сообщение всем игрокам
+                (when (= response "All players are ready. The game is starting!")
+                  ;; Логика для начала игры
+                  (println "The game has started!")))
+              (.flush *err*)
+              (print player/prompt) (flush)
+              (recur (read-line))))
+          (finally (cleanup player-name)))))))
+
+; срабатывает 3 мейна снизу вверх
 
 (defn -main
   ([port dir]
-     (rooms/add-rooms dir)
      (defonce server (socket/create-server (Integer. port) mire-handle-client))
      (println "Launching Mire server on port" port))
+
   ([port] (-main port "resources/rooms"))
-  ([] (-main 3333)))
+
+  ([] (-main 3334)))
